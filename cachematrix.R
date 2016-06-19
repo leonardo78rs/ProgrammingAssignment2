7951abce5489ccf2d838ebb22b817d5acb2b245b
leonardo78rs <leonardo78rs@hotmail.com>
2016-06-19 10:35:11 -0300

    leonardo78rs
    leonardo78rs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R Programming - Assignment Week 3 
+## 
+## Leonardo Mendes de Oliveira 19/06/2016.
+##
+## Code example: 
+##   abc<- makeCacheMatrix(cbind(c(1,1,2,1),c(1,3,5,1),c(3,3,3,1),c(1,2,3,1)))
+##   cacheSolve(abc) # first = calc
+##   cacheSolve(abc) # second = cache
+## 
 
-## Write a short comment describing this function
+## makeCacheMatrix
+#Creates a special matrix object that can cache its inverse.
+#1.  set the value of the matrix
+#2.  get the value of the matrix
+#3.  set the value of the inverse
+#4.  get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinve <- function(solve) i <<- solve
+  getinve <- function() i
+  list(set = set, get = get, setinve = setinve, getinve = getinve)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+### cacheSolve 
+## Return a matrix that is the inverse of 'x'
+## This function calculates the inverse of the special matrix created with the above function. 
+## The first time, computes the inverse matrix. After cached, no need to calculate.
+## 
+## The matrix provided must be reversible.
+## Error in x$getinv : $ operator is invalid for atomic vectors
+
+
+cacheSolve <- function(x, ...) { 
+  i<- x$getinve()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i<- solve(data, ...)
+  x$setinve(i)
+  i
+  
 }
